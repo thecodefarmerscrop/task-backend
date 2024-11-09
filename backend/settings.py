931121e1087159bcf3756c85791e1ee4a13eb42e
David Smith <dave@rabbit.beer>
2024-11-09 02:10:14 -0500

    cors

--- backend/settings.py
+++ backend/settings.py
@@ -146,6 +146,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
 #"http://localhost:3000",
 "https://task-api.crimetea.com",
+"https://tasklist.crimetea.com",
 ]
 
 #Added but not Documented-------------------------------------------
